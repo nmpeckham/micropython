714bdf48b5ee31fc8534c6d10c064e62cfda0010
Nathan Peckham <nathan@raysonplace.ca>
2025-02-06 01:07:44 -0330

    PuntPad version

--- ports/rp2/boards/W5100S_EVB_PICO/mpconfigboard.h
+++ ports/rp2/boards/W5100S_EVB_PICO/mpconfigboard.h
@@ -1,12 +1,21 @@
 // Board config for Wiznet W5100S-EVB-Pico.
 
 // Board and hardware specific configuration
-#define MICROPY_HW_BOARD_NAME               "W5100S-EVB-Pico"
-#define MICROPY_HW_FLASH_STORAGE_BYTES      (1408 * 1024)
+#define MICROPY_HW_BOARD_NAME               "PuntPad"
+#define MICROPY_HW_FLASH_STORAGE_BYTES      (848 * 1024)
 
 // Enable networking.
 #define MICROPY_PY_NETWORK                  (1)
-#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "W5100S-EVB"
+#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT "PuntPad"
+#define CYW43_USE_SPI (1)
+#define CYW43_LWIP (1)
+#define CYW43_GPIO (1)
+#define CYW43_SPI_PIO (1)
+
+#define MICROPY_HW_PIN_EXT_COUNT    CYW43_WL_GPIO_COUNT
+
+#define MICROPY_HW_PIN_RESERVED(i) ((i) == CYW43_PIN_WL_HOST_WAKE || (i) == CYW43_PIN_WL_REG_ON)
+
 
 // Wiznet HW config.
 #define MICROPY_HW_WIZNET_SPI_ID            (0)
